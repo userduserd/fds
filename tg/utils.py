6e0cbaa0c29fee83f5443c4e2901be010e86b5db
userduserd <dino_server@proton.me>
2025-01-30 04:43:29 +0600

    fdg

--- tg/utils.py
+++ tg/utils.py
@@ -14,6 +14,6 @@ async def invoice_checker(bot):
                 text = invoice_changer_text.format(uniq=invoice.uniq_id, req=invoice.req.req, amount=invoice.amount,
                                                    bank_name=invoice.req.name)
                 builder.add(InlineKeyboardButton(text="Принять", callback_data=f"accept_inv_{invoice.id}"))
-                await bot.send_message(chat_id=invoice.req.user.user_id, text=text)
+                await bot.send_message(chat_id=invoice.req.user.user_id, text=text, reply_markup=builder.as_markup())
                 invoice.new_invoice = False
                 invoice.save()
