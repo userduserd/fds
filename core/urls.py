2e67c28c8f6581a023a4a07c16d2cb0fcdcfc289
userduserd <dino_server@proton.me>
2025-01-29 23:43:37 +0600

    gdfg

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tg.views import GetActiveReqView
+from tg.views import GetActiveReqView, GetCourseView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('req/', GetActiveReqView.as_view(), name='get-active-req'),
+    path('course/', GetCourseView.as_view(), name='get-course')
 ]
 
