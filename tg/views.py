a23ffe4d7dc2bf738b53432c0b21b23eb7ca1a69
userduserd <dino_server@proton.me>
2025-01-30 03:06:22 +0600

    fdg

--- tg/views.py
+++ tg/views.py
@@ -9,7 +9,7 @@ class GetActiveReqView(APIView):
         try:
             active_req = Req.objects.filter(is_active=True).order_by("?").first()
             invoice = Invoice.objects.create(amount=amount, changer=active_req.user)
-
+            invoice.save()
             if not active_req:
                 return Response({"error": "No active req found"}, status=status.HTTP_404_NOT_FOUND)
 
